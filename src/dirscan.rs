8d8700b2b1b7c0be92c00cdca2eceac8655d8bba
Per Arneng <per.arneng@scalebit.com>
2025-02-25 12:35:03 +0100

    iterating works but needs improvements

--- src/dirscan.rs
+++ src/dirscan.rs
@@ -1,31 +1,46 @@
 
 use std::path::PathBuf;
 use log::info;
+use regex::Regex;
 use crate::io_utils::{simple_patterns_to_regexps, to_io_err_with_context};
 
 pub fn get_files(dir: &PathBuf, patterns: &[String])
     -> Result<Vec<PathBuf>, Box<dyn std::error::Error>> {
 
-    info!("get_files: searching for files in {:?}", dir);
-
-
+    info!("get_files: compiling patterns to regular expressions: {:?}", patterns);
     let compiled_patterns =
         simple_patterns_to_regexps(patterns)?;
 
-    let mut files = Vec::new();
-    Ok(files)
+
+    info!("get_files: searching for files in {:?}", dir);
+
+    let found_files: Vec<PathBuf> = walk_path(dir)
+        .filter(is_file)
+        .filter(matches_patterns(&compiled_patterns))
+        .collect();
+
+    info!("get_files: found: {:?} files", found_files.len());
+
+    Ok(found_files)
 }
 
-fn scan_dir(dir: &PathBuf, patterns: &[String]) -> Vec<PathBuf> {
-    let mut files = Vec::new();
-    for entry in std::fs::read_dir(dir).unwrap() {
-        let entry = entry.unwrap();
-        let path = entry.path();
-        if path.is_dir() {
-            files.append(&mut scan_dir(&path, patterns));
-        } else {
-            files.push(path);
-        }
-    }
-    files
+fn is_file(path: &PathBuf) -> bool {
+    path.is_file()
+}
+
+fn matches_patterns<'a>(compiled_patterns: &'a [Regex]) -> impl Fn(&PathBuf) -> bool + 'a {
+    move |path: &PathBuf| compiled_patterns.iter()
+                .any(|re| re.is_match(path.to_str().unwrap()))
+}
+
+fn walk_path(path_buf: &PathBuf)
+             -> impl Iterator<Item = PathBuf> {
+    walkdir::WalkDir::new(path_buf)
+        .into_iter()
+        .filter_map(|entry| {
+            if entry.is_err() {
+                return None;
+            }
+            Some(entry.unwrap().path().to_path_buf())
+        })
 }
